994b5ad1e71a80b9079021cf9b6a46f8ce36bf5e
Álmos Szabó <almos.szabo@lombiq.com>
2017-10-26 00:06:07 +0200

    PositEnvironment added.
    --HG--
    branch : issue/HAST-136

---
+++ Posit/Posit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lombiq.Arithmetics.Posit
+{
+    struct Posit
+    {
+        private readonly PositEnvironment _environment;
+        public BitMask PositBits { get; }
+
+        #region Posit structure
+
+        public byte MaximumExponentSize => _environment.MaximumExponentSize;
+
+        public ushort Size => _environment.Size;
+
+        #endregion
+
+        #region Posit constructors
+
+        public Posit(PositEnvironment environment)
+        {
+            _environment = environment;
+
+            PositBits = new BitMask(_environment.Size);
+        }
+
+        public Posit(PositEnvironment environment, BitMask bits)
+        {
+            _environment = environment;
+
+            PositBits = BitMask.FromImmutableArray(bits.Segments, _environment.Size);
+        }
+
+        //public Posit(PositEnvironment environment, int value)
+        //{
+        //    _environment = environment;
+
+
+        //}
+
+        //public Posit(PositEnvironment environment, uint value)
+        //{
+        //    _environment = environment;
+
+
+        //}
+
+        #endregion
+
+        #region Methods to handle parts of the Posit 
+
+        
+
+        #endregion
+    }
+}
---
+++ Posit/PositEnvironment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lombiq.Arithmetics.Posit
+{
+    public class PositEnvironment
+    {
+        public byte MaximumExponentSize { get; } //es
+
+        public ushort Size { get; } //nbits
+
+        public uint Useed { get; }
+
+        public ushort FirstRegimeBitPosition { get; }
+        public BitMask SignBitMask { get; }
+
+        public BitMask FirstRegimeBitBitMask { get; }
+
+
+        public PositEnvironment(byte size, byte maximumExponentSize)
+        {
+            Size = size;
+            MaximumExponentSize = maximumExponentSize;
+
+            Useed = (uint)2 << (2 << MaximumExponentSize);
+            SignBitMask = new BitMask(Size).SetOne((ushort)(Size - 1));
+            FirstRegimeBitPosition = (ushort)(Size - 2);
+            FirstRegimeBitBitMask = new BitMask(Size).SetOne(FirstRegimeBitPosition);
+        }
+
+    }
+}
