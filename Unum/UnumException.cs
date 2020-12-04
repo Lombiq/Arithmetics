d88804298d935b19c7f21d906b5429bc64b39585
Dávid El-Saig <david.el-saig@lombiq.com>
2020-12-04 06:26:59 +0100

    Fix exceptions.

--- Unum/UnumException.cs
+++ Unum/UnumException.cs
@@ -1,11 +1,20 @@
-﻿using System;
+using System;
+using System.Runtime.Serialization;
 
 namespace Lombiq.Arithmetics
 {
+    [Serializable]
     public class UnumException : Exception
     {
-        public UnumException(string message) : base(message) { }
+        public UnumException(string message)
+            : base(message) { }
 
-        public UnumException(string message, Exception innerException) : base(message, innerException) { }
+        public UnumException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public UnumException() { }
+
+        protected UnumException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext) { }
     }
 }
