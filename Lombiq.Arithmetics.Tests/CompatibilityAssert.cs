00b207f56029be240283e51d83d75be7e7235b09
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-20 19:40:03 +0100

    Revert CompatibilityAssert.

--- Lombiq.Arithmetics.Tests/CompatibilityAssert.cs
+++ Lombiq.Arithmetics.Tests/CompatibilityAssert.cs
@@ -1,19 +1,25 @@
-using Shouldly;
+using Xunit.Sdk;
 
 namespace Lombiq.Arithmetics.Tests
 {
     public static class CompatibilityAssert
     {
-        public static void AreEqual<T>(T actual, T expected) => actual.ShouldBe(expected);
+        public static void AreEqual<T>(T actual, T expected) => Xunit.Assert.Equal(expected, actual);
 
-        public static void AreEqual(uint actual, int expected) => actual.ShouldBe((uint)expected);
+        public static void AreEqual(uint actual, int expected) => Xunit.Assert.Equal((uint)expected, actual);
 
-        public static void AreEqual(int actual, uint expected)
+        public static void AreEqual(int actual, uint expected) => Xunit.Assert.Equal(expected, (uint)actual);
+
+        public static void AreEqual<T>(T actual, T expected, string userMessage)
         {
-            var actualUInt = (uint)actual;
-            actualUInt.ShouldBe(expected);
+            try
+            {
+                Xunit.Assert.Equal(expected, actual);
+            }
+            catch (EqualException)
+            {
+                Xunit.Assert.True(false, userMessage);
+            }
         }
-
-        public static void AreEqual<T>(T actual, T expected, string userMessage) => actual.ShouldBe(expected, userMessage);
     }
 }
